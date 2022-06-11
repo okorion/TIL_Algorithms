0c92bf94f8a92a6f1fec165bd4bdc2c30c240f32
okorion <97646070+okorion@users.noreply.github.com>
2022-06-11 22:59:21 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -6,7 +6,7 @@ temp_list = list(map(int, sys.stdin.readline().split()))
 res = 0
 
 for i in range(len(temp_list)-2):
-    if i > 0 and temp_list[i] == temp_list[i-1]:
+    if temp_list[i] == temp_list[i-1]:
         continue
     for j in range(i+1,len(temp_list)-1):
         if j > i+1 and temp_list[j] == temp_list[j-1]:
