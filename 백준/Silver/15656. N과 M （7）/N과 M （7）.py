56515b44ad78f48b01fa75910b84b35ee3cfd162
okorion <97646070+okorion@users.noreply.github.com>
2022-08-14 23:30:13 +0900

    [Silver III] Title: N과 M (7), Time: 1772 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+temp_list = [0] + sorted(list(map(int, input().split())))
+result_list = []
+
+
+def backtracking(k, n, m):
+    if len(result_list) == m:
+        return print(" ".join(map(str, result_list)))
+
+    for num in range(1, n+1):
+        result_list.append(temp_list[num])
+        backtracking(k+1, n, m)
+        result_list.pop()
+
+
+backtracking(0, N, M)
