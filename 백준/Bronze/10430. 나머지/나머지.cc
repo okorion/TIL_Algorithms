391788c5253f7c769d47dce044d9d8336c22d83f
okorion <97646070+okorion@users.noreply.github.com>
2022-09-26 21:36:01 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b, c;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    
+    printf("%d\n", (a+b)%c);
+    printf("%d\n", ((a%c)+(b%c))%c);
+    printf("%d\n", (a*b)%c);
+    printf("%d", (a%c)*(b%c)%c);
+    return 0;
+    ;
+}
