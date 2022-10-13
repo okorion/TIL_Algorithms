c48ab2ea42ac529761611fea240dfebe7ed8d594
okorion <97646070+okorion@users.noreply.github.com>
2022-10-13 20:17:07 +0900

    [Bronze V] Title: 개, Time: 176 ms, Memory: 16288 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) {
+        String a = "|\\_/|\n";
+        String b = "|q p|   /}\n";
+        String c = "( 0 )\"\"\"\\\n";
+        String d = "|\"^\"`    |\n";
+        String e = "||_/=\\\\__|";
+        
+        System.out.print(a + b + c + d + e);
+        }
+    }
