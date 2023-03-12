13ae00c72602e6be556a8f2ddef37c05dbd85576
okorion <97646070+okorion@users.noreply.github.com>
2023-03-12 18:34:27 +0900

    [level 2] Title: 올바른 괄호, try catch 예외처리 알고리즘 활용 코드 추가

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,3 +1,4 @@
+#1
 def solution(s):
     stack = []
     flag = True
@@ -15,3 +16,25 @@ def solution(s):
         flag = False
     
     return flag
+
+
+#2 리스트에서 pop() 했을 때 안에 원소가 존재하지 않을 경우 if len(리스트) == 0: X => try/except로 예외처리 O
+
+def is_pair(s):
+    st = list()
+    for c in s:
+        if c == '(':
+            st.append(c)
+
+        if c == ')':
+            try:
+                st.pop()
+            except IndexError:
+                return False
+
+    return len(st) == 0
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print( is_pair("(hello)()"))
+print( is_pair("()()()"))
