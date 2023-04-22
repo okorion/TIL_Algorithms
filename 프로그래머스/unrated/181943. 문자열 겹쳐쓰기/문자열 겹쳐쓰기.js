d65d8ff21538f710a886f03652e80422c9c8239d
okorion <97646070+okorion@users.noreply.github.com>
2023-04-22 19:29:52 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,22 @@
+function solution(my_string, overwrite_string, s) {
+    var answer = '';
+    
+    for (let i = 0; i < s; i++) {
+        answer += my_string[i]
+    }
+    
+    answer += overwrite_string
+    
+    if (overwrite_string.length + s < my_string.length) {
+        let tmp = my_string.length - (overwrite_string.length + s)
+        
+        for (let i = my_string.length - tmp; i < my_string.length; i++) {
+            answer += my_string[i]
+        }
+    }
+    
+    
+    
+    
+    return answer;
+}
