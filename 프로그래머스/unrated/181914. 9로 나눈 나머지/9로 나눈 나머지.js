05159e9fb77fd0f312ce8d66dedbf5527a6c1e65
okorion <97646070+okorion@users.noreply.github.com>
2023-05-02 20:52:16 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -0,0 +1,11 @@
+function solution(number) {
+    var answer = 0;
+    
+    for (let i = 0; i < number.length; i++) {
+        answer += Number(number[i])
+    }
+    
+    answer %= 9
+    
+    return answer;
+}
