8888c43d491088a26b0c9897dc4cc45517c5fed5
okorion <97646070+okorion@users.noreply.github.com>
2023-05-27 12:34:03 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    var answer = 0;
+    
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] < 0) {
+            return i
+        } else if (i === num_list.length - 1) {
+            return -1
+        }
+    }
+    
+    return answer;
+}
