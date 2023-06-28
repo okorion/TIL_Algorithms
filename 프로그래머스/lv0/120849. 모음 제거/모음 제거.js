12369d82e154e21ae5e1468fd86b2c99c01c227c
okorion <97646070+okorion@users.noreply.github.com>
2023-06-28 12:45:41 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var answer = '';
+    
+    for (let i = 0; i < my_string.length; i++) {
+        if (!["a", "e", "i", "o", "u"].includes(my_string[i])) answer += my_string[i]
+    }
+    
+    return answer;
+}
