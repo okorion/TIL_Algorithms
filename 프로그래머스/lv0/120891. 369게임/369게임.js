d45a2fe3560b3de1b149903686e85349a0bef376
okorion <97646070+okorion@users.noreply.github.com>
2023-07-03 00:22:03 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -0,0 +1,13 @@
+function solution(order) {
+    var answer = 0;
+    
+    order = String(order)
+    
+    for (let i = 0; i < order.length; i++) {
+        if (order[i] === "3" || order[i] === "6" || order[i] === "9") {
+            answer += 1
+        }
+    }
+    
+    return answer;
+}
