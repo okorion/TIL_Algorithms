c663016acc39e39bcbd3b93409000e11801e2baa
okorion <97646070+okorion@users.noreply.github.com>
2023-07-05 12:44:49 +0900

    [level 0] Title: 약수 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -2,7 +2,7 @@ function solution(n) {
     let answer = [];
     
     for (let i = 1; i < n + 1; i++) {
-        if (!(n % i)) {answer.push(i)}
+        if (!(n % i)) answer.push(i)
     }
     return answer;
 }
