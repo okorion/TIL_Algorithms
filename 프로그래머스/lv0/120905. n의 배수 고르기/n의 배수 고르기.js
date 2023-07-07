3bb9eabd7d2d3d2ff360b52ffc07eeabcf15ba6a
okorion <97646070+okorion@users.noreply.github.com>
2023-07-07 13:14:38 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -0,0 +1,11 @@
+function solution(n, numlist) {
+    var answer = [];
+    
+    for (let num of numlist) {
+        if (num % n === 0) {
+            answer.push(num)
+        }
+    }
+    
+    return answer;
+}
